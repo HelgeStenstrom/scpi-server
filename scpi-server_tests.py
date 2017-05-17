4179f40742c01dd779354268e29c44264585e79b
Helge Stenström <h.stenstrom@gmail.com>
2017-05-17 22:26:59 +0200

    Förtydligat test något

--- scpi-server_tests.py
+++ scpi-server_tests.py
@@ -2,32 +2,44 @@ import scpiserver as ss
 import unittest
 import socket
 
+__author__ = "Helge Stenström"
 
 class SomeTests(unittest.TestCase):
     def setUp(self):
         self.server_address = ('localhost', 9876)
+        self.server = ss.SCPIServerExample(self.server_address)
+
+    def tearDown(self):
+        self.server.server_close()
 
     def testPass(self):
+        "Pass this test"
         pass
 
     def testCreate(self):
-        theServer = ss.SCPIServerExample(self.server_address)
+        "Create a server. Hopefully, it's destroyed by garbage collection."
+        theServer = self.server
+        self.assertEqual(1,1)
 
     def testC(self):
-        server = ss.SCPIServerExample(self.server_address)
-        print("server created")
+        "Start both a server and a client."
+        server = self.server
 
-        #print("server started")
         # todo: gör en klient, som kan prata med servern.
         client = socket.create_connection(self.server_address)
-        print("client created")
 
         client.close()
-        print("client closed")
+        # print("client closed")
+        
+        # Enligt dokumentationen måste man ha två trådar. I den ena ska man ha
+        # serve_forever(), och i den andra kan man göra shutdown(). 
+        # Jag tar bort anrop till shutdown(), tills vidare.
+        # Annars får man deadlock.
+        # server.shutdown()
 
-        # Det här verkar inte hända. Varför? Är inte servern startad?
-        server.shutdown()
-        print("server shut down")
-        server.server_close()
-        print("server closed")
-        #theServer.
+        self.assertEqual(1,1)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
